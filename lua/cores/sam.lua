e4920c23ddaf686a6d0103d9caf4ca1158df2d52
Ryan H. <ryanoutcome@gmail.com>
2025-09-08 08:27:37 -0500

    Implement pLib cores

---
+++ lua/cores/sam.lua
@@ -0,0 +1,19 @@
+-- https://www.gmodstore.com/market/view/sam
+
+return "SAM", {
+	Valid = function()
+		return istable(sam)
+	end,
+
+	Ban = function(Player, Reason, Time)
+		return sam.player.ban(Player, Time, Reason)
+	end,
+	
+	BanID = function(SID, Reason, Time)
+		return sam.player.ban_id(SID, Time, Reason)
+	end,
+	
+	Kick = function(Player, Reason)
+		return sam.player.kick_id(Player:SteamID64(), Reason)
+	end
+}
---
+++ lua/cores/xadmin.lua
@@ -0,0 +1,21 @@
+-- https://github.com/TheXYZNetwork/xAdmin
+
+return "xAdmin", {
+	Valid = function()
+		return istable(xAdmin)
+	end,
+
+	Ban = function(Player, Reason, Time)
+		return xAdmin.Database.CreateBan(Player:SteamID64(), Player:Nick(), "", "", Reason, Time * 60, function() end)
+	end,
+	
+	BanID = function(SID, Reason, Time)
+		return xAdmin.Database.CreateBan(SID, "Unknown", "", "", Reason, Time * 60, function() end)
+	end,
+	
+	Kick = function(Player, Reason)
+		-- Wrapper with fancy print message.
+		-- https://github.com/TheXYZNetwork/xAdmin/blob/d54527de884390758999469e1bdab42f428e4342/lua/xadmin/commands/discipline.lua#L33
+		return game.KickID(Player:SteamID64(), Reason)
+	end
+}
