1be820ec3b80f0f11e87e7d21200e7011e70d054
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2025-11-04 10:11:12 -0600

    Fix grammar issue with QAC honeypot

--- lua/tac/honeypots/cl_qac.lua
+++ lua/tac/honeypots/cl_qac.lua
@@ -2,17 +2,17 @@ _G.QAC = {}
 
 setmetatable(_G.QAC, {
     __eq = function(...)
-		TAC.Honeypot.Alert("QAC equals")
+		TAC.Honeypot.Alert("QAC: equals")
         return
     end,
 	
 	__index = function(...)
-		TAC.Honeypot.Alert("QAC index")
+		TAC.Honeypot.Alert("QAC: index")
 		return
 	end,
 	
 	__newindex = function(...)
-		TAC.Honeypot.Alert("QAC new index")
+		TAC.Honeypot.Alert("QAC: new index")
 		return
 	end
 })
