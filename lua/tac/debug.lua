35d5f2646718265f5d07a8ea1a1b2cc1826b7823
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2025-11-28 12:09:48 -0600

    Update debug tests before culling this file

--- lua/tac/debug.lua
+++ lua/tac/debug.lua
@@ -21,6 +21,47 @@ concommand.Add("tac_cout", function()
 	)
 end)
 
+local Total = 0
+
+local function 	Callback(Table, Checked)
+	if not istable(Table) then 
+		return
+	end
+
+	Checked = Checked or {
+		[Table] = true
+	}
+
+	for k,v in pairs(Table) do 
+		if Checked[v] then
+			continue
+		end
+
+		Checked[v] = true
+		
+		if istable(v) then
+			Callback(v, Checked)
+		end
+
+		Total = Total + 1
+	end
+end
+
+concommand.Add("tac_cout_recursive", function()
+	PrintTable(TAC)
+
+	Total = 0 
+
+	Callback(TAC)
+
+	TAC.Print(
+		PRINT_DEBUG,
+		"Debug",
+		"Dumped %i indexes from TAC",
+		Total
+	)
+end)
+
 concommand.Add("tac_reload", function()
 	include("autorun/server/sv_trinity.lua")
 	
