18dff04034276d558234336211f11fc4f185a765
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2025-12-29 11:47:23 -0600

    SlobBot & Sethhack globals (#101)

--- lua/tac/lists/cl_globals.lua
+++ lua/tac/lists/cl_globals.lua
@@ -18,5 +18,36 @@ return {
     {Name = "dickwrap", Flag = "dickwrap"},
 
     -- _fhook
-    {Name = "_fhook_changename", Flag = "_fhook"}
+    {Name = "_fhook_changename", Flag = "_fhook"},
+
+    -- SlobBot
+    {Name = "SlobMegaAIM", Flag = "SlobBot"},
+    {Name = "GetSlobBotTarget", Flag = "SlobBot"},
+    {Name = "AntiBanOn", Flag = "SlobBot"},
+    {Name = "BotVisible", Flag = "SlobBot"},
+    {Name = "WOTS", Flag = "SlobBot"},
+    {Name = "SlobTeamAllowed", Flag = "SlobBot"},
+    {Name = "SafeAIM", Flag = "SlobBot"},
+    {Name = "SlobNotify", Flag = "SlobBot"},
+    {Name = "EzekBuddyAllowed", Flag = "SlobBot"},
+    {Name = "SlobBotAllowed", Flag = "SlobBot"},
+    {Name = "HeadPosition", Flag = "SlobBot"},
+    
+    -- Sethhack
+    {Name = "SH_SETCVAR", Flag = "Sethhack"},
+    {Name = "SH_SETNAME", Flag = "Sethhack"},
+    {Name = "SH_PURECC", Flag = "Sethhack"},
+    {Name = "SH_LUARUN", Flag = "Sethhack"},
+    {Name = "SH_SETSPEED", Flag = "Sethhack"},
+    {Name = "SH_COMMANDNUMBER", Flag = "Sethhack"},
+    {Name = "SH_ISDORMANT", Flag = "Sethhack"},
+    {Name = "SH_MODVER", Flag = "Sethhack"},
+    {Name = "SH_READFILE", Flag = "Sethhack"},
+    {Name = "SH_WRITEFILE", Flag = "Sethhack"},
+    {Name = "SH_SUPPRESSIPLOGS", Flag = "Sethhack"},
+    {Name = "SH_RUNSCRIPTS", Flag = "Sethhack"},
+    {Name = "SH_REGREAD", Flag = "Sethhack"},
+    {Name = "SH_REGWRITE", Flag = "Sethhack"},
+    {Name = "SH_hl2_ucmd_getprediction", Flag = "Sethhack"},
+    {Name = "SH_hl2_shotmanip", Flag = "Sethhack"}
 }
