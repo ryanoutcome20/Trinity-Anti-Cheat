35a7435f7ce25695b64f31ee21788dc3e94628ae
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-01-06 00:37:27 -0600

    Add Spectre security script to static blacklist

--- lua/tac/lists/cl_static.lua
+++ lua/tac/lists/cl_static.lua
@@ -7,5 +7,6 @@ return {
     {Name = "Coffee", Path = "GAME", Directory = "coffee", Type = TYPE_DIR},
     {Name = "D3C", Path = "DATA", Directory = "icefuse/content/colorpicker", Type = TYPE_DIR},
     {Name = "Majestic", Path = "GAME", Directory = "majestic", Type = TYPE_DIR},
-    {Name = "s0lame", Path = "GAME", Directory = "lua/s0lame", Type = TYPE_DIR}
+    {Name = "s0lame", Path = "GAME", Directory = "lua/s0lame", Type = TYPE_DIR},
+    {Name = "Spectre", Path = "GAME", Directory = "lua/spectre", Type = TYPE_DIR}
 }
