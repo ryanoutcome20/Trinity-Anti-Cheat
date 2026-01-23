58e13dda29c82d660bd9d5c676da931548bfeb64
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-01-23 07:09:17 -0600

    Add some self checks to reporter

--- lua/tac/modules/reporter/sv_reporter.lua
+++ lua/tac/modules/reporter/sv_reporter.lua
@@ -1,4 +1,11 @@
 function TAC.Reporter(Stage, Player, OS, Sensitivity, Resolution)
+    OS = isstring(OS) and OS or "Windows"
+    Sensitivity = isnumber(Sensitivity) and Sensitivity or 3
+    Resolution = {
+        ScrW = isnumber(Resolution.ScrW) and Resolution.ScrW or 1920,
+        ScrH = isnumber(Resolution.ScrH) and Resolution.ScrH or 1080
+    }
+
     if OS == "OSX" then
         Player:Set("OSX", true)
     elseif OS == "Linux" then
