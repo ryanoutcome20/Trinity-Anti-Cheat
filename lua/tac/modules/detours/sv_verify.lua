3c4b9a1f01137aefabb3b2e537a0afe6769a0b52
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-02-02 11:38:25 -0600

    Detour verification ban message improvements & more detections

--- lua/tac/modules/detours/sv_verify.lua
+++ lua/tac/modules/detours/sv_verify.lua
@@ -1,18 +1,20 @@
 function TAC.Detours.CheckC(Player, Object)
 	if not isnumber(Object.linedefined) or not isnumber(Object.lastlinedefined) then
-		return TAC.Detours.Wrapper(Player, "Emulated C [line not number]")
+		return TAC.Detours.Wrapper(Player, "Emulated C [lines; number expected got %s and %s]", type(Object.linedefined), type(Object.lastlinedefined))
 	elseif not isstring(Object.j_linedefined) then
-		return TAC.Detours.Wrapper(Player, "Emulated C [jit line not string]")
+		return TAC.Detours.Wrapper(Player, "Emulated C [jit lines; string expected got %s]", type(Object.j_linedefined))
 	elseif not isstring(Object.what) then
-		return TAC.Detours.Wrapper(Player, "Emulated C [what not string]")
+		return TAC.Detours.Wrapper(Player, "Emulated C [what; string expected got %s]", type(Object.what))
 	end
 
-	if Object.linedefined ~= -1 or Object.linedefined ~= Object.lastlinedefined then
+	if Object.linedefined ~= -1 then
+		return TAC.Detours.Wrapper(Player, "Emulated C [line valid: %i]", Object.linedefined)
+	elseif Object.linedefined ~= Object.lastlinedefined then
 		return TAC.Detours.Wrapper(Player, "Emulated C [%i ~= %i]", Object.linedefined, Object.lastlinedefined)
 	end
 	
 	if Object.j_linedefined ~= "ld" then
-		return TAC.Detours.Wrapper(Player, "Emulated C [line: %s]", TAC.Fix(Object.j_linedefined))
+		return TAC.Detours.Wrapper(Player, "Emulated C [jit line valid: %s]", TAC.Fix(Object.j_linedefined))
 	end
 	
 	if Object.what == "main" then
@@ -22,13 +24,19 @@ end
 
 function TAC.Detours.CheckLua(Player, Object)
 	if not isstring(Object.short_src) then
-		return TAC.Detours.Wrapper(Player, "Emulated Lua [short source not string]")
+		return TAC.Detours.Wrapper(Player, "Emulated Lua [short_src; string expected got %s]", type(Object.short_src))
 	elseif not isstring(Object.what) then
-		return TAC.Detours.Wrapper(Player, "Emulated Lua [short source not string]")
+		return TAC.Detours.Wrapper(Player, "Emulated Lua [what; string expected got %s]", type(Object.what))
 	elseif not isnumber(Object.lastlinedefined) then
-		return TAC.Detours.Wrapper(Player, "Emulated Lua [last line not number]")
+		return TAC.Detours.Wrapper(Player, "Emulated Lua [last line; number expected got %s]", type(Object.lastlinedefined))
 	elseif not isnumber(Object.linedefined) or not isnumber(Object.j_linedefined) then
-		return TAC.Detours.Wrapper(Player, "Emulated Lua [lines not number]")
+		return TAC.Detours.Wrapper(Player, "Emulated Lua [lines; number expected got %s and %s]", type(Object.linedefined), type(Object.j_linedefined))
+	end
+
+	if Object.j_linedefined ~= Object.linedefined then
+		return TAC.Detours.Wrapper(Player, "Emulated Lua [lines; %i ~= %i]", Object.j_linedefined, Object.linedefined)
+	elseif Object.linedefined > Object.lastlinedefined then
+		return TAC.Detours.Wrapper(Player, "Emulated Lua [last over line; %i > %i]", Object.lastlinedefined, Object.linedefined)
 	end
 
 	local Cache = TAC.Detours.Get(Object.short_src)
