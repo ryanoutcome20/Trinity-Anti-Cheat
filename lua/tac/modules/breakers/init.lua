ffa3daa464cf263095e6576ae0436b705dbc99d7
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-02-13 01:55:33 -0600

    refactor: Remove Spread Desyncer (#132)

--- lua/tac/modules/breakers/init.lua
+++ lua/tac/modules/breakers/init.lua
@@ -1,10 +1,9 @@
 TAC.Breakers = { }
 
 if SERVER then
-	include("sv_clicker.lua")
 	include("sv_interpolated.lua")
+	include("sv_clicker.lua")
 	include("sv_pvs.lua")
-	include("sv_spread.lua")
 	
 	return {
 		"init.lua",
