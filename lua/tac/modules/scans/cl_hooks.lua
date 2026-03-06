6812516415703548c0d8550a9a274751368f1b2e
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-03-06 16:46:32 -0600

    style: Adjust name of hook list to match standard

--- lua/tac/modules/scans/cl_hooks.lua
+++ lua/tac/modules/scans/cl_hooks.lua
@@ -1,4 +1,4 @@
-local Hooks = TAC.Lists.Merge("Hooks")
+local List = TAC.Lists.Merge("Hooks")
 
 local Config = TAC.Config.Scans.Hooks
 
@@ -7,13 +7,13 @@ local function Scan()
 		return
 	end
 	
-    if not Hooks then
+    if not List then
         return TAC.Flag("Hooks", "Bad Hook [missing]")
     end 
 	
 	local Table = hook.GetTable()
 	
-	for k, Object in ipairs(Hooks) do 
+	for k, Object in ipairs(List) do 
 		if Table[Object.Hook] and Table[Object.Hook][Object.Name] then
 			TAC.Flag("Hooks", "Bad Hook [hook: %s; name: %s]", Object.Hook, Object.Name)
 			break
