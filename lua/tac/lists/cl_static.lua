6fd988e3809a75e1dae735cd626b9c344c9c828e
Vectivus <pearse614@gmail.com>
2026-04-30 22:36:49 +0100

    feat: Added Kirkware file paths to blacklist

--- lua/tac/lists/cl_static.lua
+++ lua/tac/lists/cl_static.lua
@@ -8,5 +8,6 @@ return {
     {Name = "D3C", Path = "DATA", Directory = "icefuse/content/colorpicker", Type = TYPE_DIR},
     {Name = "Majestic", Path = "GAME", Directory = "majestic", Type = TYPE_DIR},
     {Name = "s0lame", Path = "GAME", Directory = "lua/s0lame", Type = TYPE_DIR},
-    {Name = "Spectre", Path = "GAME", Directory = "lua/spectre", Type = TYPE_DIR}
+    {Name = "Spectre", Path = "GAME", Directory = "lua/spectre", Type = TYPE_DIR},
+    {Name = "Kirkware", Path = "DATA", Directory = "kirkware/cache", Type = TYPE_DIR}
 }
