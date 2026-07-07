59d167d7a73f89468ede3a51120c67519f151290
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-07-07 03:08:51 -0500

    feat: Update global module to use internal timers

--- lua/tac/modules/scans/cl_globals.lua
+++ lua/tac/modules/scans/cl_globals.lua
@@ -3,23 +3,22 @@ local List = TAC.Lists.Merge("Globals")
 local Config = TAC.Config.Scans.Globals
 
 local function Scan()
-	if not Config.Enabled then
-		return
-	end
-
     if not List then
         return TAC.Flag("Globals", "Bad Global [missing]")
     end 
 
+	local Flagged = false
+	
 	for k, Data in ipairs(List) do
 		if _G[Data.Name] ~= nil then
 			TAC.Flag("Globals", "Bad Global [name: %s; suspect: %s]", Data.Name, Data.Flag)
+			Flagged = true
 		end
 	end
 
-	timer.Simple(Config.Delay, Scan)
+	return Flagged
 end
 
 hook.Add("TAC.Initialize", "TAC.Globals", function()
-	timer.Simple(Config.Delay, Scan)
+	TAC.Timing.New(Scan, Config, true)
 end)
