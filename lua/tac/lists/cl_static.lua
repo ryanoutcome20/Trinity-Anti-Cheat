1c296fc4b026a47b6372e03b2c173c182cf7732e
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-07-19 08:02:29 -0500

    style: Update style of the `static` list

--- lua/tac/lists/cl_static.lua
+++ lua/tac/lists/cl_static.lua
@@ -3,11 +3,18 @@ local TYPE_FILE = 1
 
 return {
     {Name = "Interstate", Path = "DATA", Directory = "interscripts/stolen", Type = TYPE_DIR},
+
     {Name = "Memoriam", Path = "GAME_PATH", Directory = "memoriam/configs", Type = TYPE_DIR},
+
     {Name = "Coffee", Path = "GAME", Directory = "coffee", Type = TYPE_DIR},
+
     {Name = "D3C", Path = "DATA", Directory = "icefuse/content/colorpicker", Type = TYPE_DIR},
+
     {Name = "Majestic", Path = "GAME", Directory = "majestic", Type = TYPE_DIR},
+
     {Name = "s0lame", Path = "GAME", Directory = "lua/s0lame", Type = TYPE_DIR},
+
     {Name = "Spectre", Path = "GAME", Directory = "lua/spectre", Type = TYPE_DIR},
+
     {Name = "Kirkware", Path = "DATA", Directory = "kirkware/cache", Type = TYPE_DIR}
 }
