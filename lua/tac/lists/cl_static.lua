e2cff0eb17ee406f2d314012f226dc81e5188b16
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-07-19 16:15:28 -0500

    feat: More `files` -> `static` list indexes

--- lua/tac/lists/cl_static.lua
+++ lua/tac/lists/cl_static.lua
@@ -34,6 +34,7 @@ return {
     {"Safari", "DATA", "crappens.txt", TYPE_FILE},
 
     {"NeonHack", "DATA", "neon_logged_ips.txt", TYPE_FILE},
+    {"NeonHack", "DATA", "NeonAuth.txt", TYPE_FILE},
 
     {"AllahCheat", "DATA", "AC", TYPE_DIR},
 
@@ -51,5 +52,100 @@ return {
 
     {"AutoAim (BenBoost)", "DATA", "ac_ips.txt", TYPE_FILE},
 
-    {"b1g", "DATA", "_b1g_settings.txt", TYPE_FILE }
+    {"b1g", "DATA", "_b1g_settings.txt", TYPE_FILE},
+
+    {"Bunnyware", "DATA", "bunnywareold.txt", TYPE_FILE},
+
+    {"Memeware", "DATA", "memeware.txt", TYPE_FILE},
+    {"Memeware (MrSquid Release)", "DATA", "$altyConfig.txt", TYPE_FILE},
+
+    {"BXS", "DATA", "BXS", TYPE_DIR},
+
+    {"Mingescript", "DATA", "mssettings.txt", TYPE_FILE},
+
+    {"Chlenix", "DATA", "chlenix.txt", TYPE_FILE},
+
+    {"ColdFire", "DATA", "ColdFire", TYPE_DIR},
+
+    {"Coma", "DATA", "Coma_ips.txt", TYPE_FILE},
+
+    {"dedpacman", "DATA", "dedpacman_config.txt", TYPE_FILE},
+
+    {"Defcon", "DATA", "d_settings.txt", TYPE_FILE},
+    {"Defcon", "DATA", "d_entities.txt", TYPE_FILE},
+    {"Defcon", "DATA", "d_whitelist.txt", TYPE_FILE},
+    {"Defcon", "DATA", "d_aimkey.txt", TYPE_FILE},
+    {"Defcon", "DATA", "d_trigkey.txt", TYPE_FILE},
+
+    {"EternalMenuHook", "DATA", "EternalMenuHook", TYPE_DIR},
+    {"MenuHook", "DATA", "menuhook", TYPE_DIR},
+
+    {"EthanBot", "DATA", "EthanBot", TYPE_DIR},
+
+    {"Exie", "DATA", "EXIE_logged_ips.txt", TYPE_FILE},
+
+    {"Snowy", "DATA", "sg_cache.txt", TYPE_FILE},
+
+    {"FartClient", "DATA", "FartClient", TYPE_DIR},
+
+    {"FuckMenu", "DATA", "my_mod", TYPE_DIR},
+
+    {"GBot", "DATA", "gbot_cahce.txt", TYPE_FILE},
+    {"GBot", "DATA", "gbot_cents.txt", TYPE_FILE},
+
+    {"gerrigscript", "DATA", "geralds_wins_and_losses.txt", TYPE_FILE},
+    {"gerrigscript", "DATA", "geralds_propkill.txt", TYPE_FILE},
+
+    {"ghack", "DATA", "ghack.txt", TYPE_FILE},
+
+    {"Hake", "DATA", "hake_ips.txt", TYPE_FILE},
+    
+    {"Helium", "DATA", "helium-config.txt", TYPE_FILE},
+
+    {"HemiHack", "DATA", "hemihack", TYPE_DIR},
+
+    {"Hermes", "DATA", "hermes_lists_friends.txt", TYPE_FILE},
+    {"Hermes", "DATA", "hermes_lists_entities.txt", TYPE_FILE},
+    {"Hermes", "DATA", "hermes_log.txt", TYPE_FILE},
+    {"Hermes", "DATA", "hermes_lists_binds.txt", TYPE_FILE},
+
+    {"Hexa", "DATA", "hexahack.txt", TYPE_FILE},
+
+    {"Jackson", "GAME", "lua/jackson_modules", TYPE_DIR},
+
+    {"K4 (v1.9b)", "DATA", "K4Auth.txt", TYPE_FILE},
+
+    {"Kata", "DATA", "Katacfg.txt", TYPE_FILE},
+
+    {"BaconBot/SH v3.0r/Hades", "DATA", "db_steamid_ip.txt", TYPE_FILE},
+
+    {"lolscript (5.9b-6.0)", "DATA", "lv9d3c.txt", TYPE_FILE},
+
+    {"lordofgear/lordhack", "DATA", "LORD.friends.txt", TYPE_FILE},
+    {"lordofgear/lordhack", "DATA", "LORDpic.txt", TYPE_FILE},
+
+    {"lucidcheats", "DATA", "lwn.txt", TYPE_FILE},
+    {"lucidcheats", "DATA", "whitytighty.txt", TYPE_FILE},
+    {"lucidcheats", "DATA", "hackerlistv2.txt", TYPE_FILE},
+    {"lucidcheats", "DATA", "spamlist.txt", TYPE_FILE},
+    {"lucidcheats", "DATA", "opt.txt", TYPE_FILE},
+    {"lucidcheats", "DATA", "rOffset.txt", TYPE_FILE},
+
+    {"mApex", "DATA", "aimbot_whitelist.txt", TYPE_FILE},
+
+    {"mechgear", "DATA", "mechgear", TYPE_DIR},
+
+    {"MicroBot", "DATA", "microbot.aimcfg.txt", TYPE_FILE},
+    {"MicroBot", "DATA", "microbot.espcfg.txt", TYPE_FILE},
+    {"MicroBot", "DATA", "microbot.misccfg.txt", TYPE_FILE},
+
+    {"MindFuc", "DATA", "MindFuc", TYPE_DIR},
+
+    {"MythX", "DATA", "MythX_Settings.txt", TYPE_FILE},
+
+    {"namejeff", "DATA", "namejeff", TYPE_DIR},
+
+    {"NeatCheat", "DATA", "Neat_cheat", TYPE_DIR},
+
+    {"TimesHack", "DATA", "timeshack", TYPE_DIR}
 }
