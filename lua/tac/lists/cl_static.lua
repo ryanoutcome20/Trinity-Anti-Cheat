ac2ad422e8daccf0f61bedd374013965537dde3a
Ryan H. <131711662+ryanoutcome20@users.noreply.github.com>
2026-07-19 08:47:34 -0500

    feat: Start moving files to static module
    Gonna be a long process so this is just the start, plus I want to make some architecture changes so I'll push half here and the rest later.

--- lua/tac/lists/cl_static.lua
+++ lua/tac/lists/cl_static.lua
@@ -11,10 +11,45 @@ return {
     {Name = "D3C", Path = "DATA", Directory = "icefuse/content/colorpicker", Type = TYPE_DIR},
 
     {Name = "Majestic", Path = "GAME", Directory = "majestic", Type = TYPE_DIR},
+    {Name = "Majestic", Path = "GAME", Directory = "lua/majestic.lua", Type = TYPE_FILE},
 
     {Name = "s0lame", Path = "GAME", Directory = "lua/s0lame", Type = TYPE_DIR},
 
     {Name = "Spectre", Path = "GAME", Directory = "lua/spectre", Type = TYPE_DIR},
 
-    {Name = "Kirkware", Path = "DATA", Directory = "kirkware/cache", Type = TYPE_DIR}
+    {Name = "Kirkware", Path = "DATA", Directory = "kirkware/cache", Type = TYPE_DIR},
+
+    {Name = "Ultimate", Path = "DATA", Directory = "ultimate/default.txt", Type = TYPE_FILE},
+    {Name = "Ultimate", Path = "DATA", Directory = "prikol.png", Type = TYPE_FILE},
+
+    {Name = "0xray v1.0", Path = "DATA", Directory = "0xray_props.txt", Type = TYPE_FILE},
+    {Name = "0xray v2.0", Path = "DATA", Directory = "0xray", Type = TYPE_DIR},
+
+    {Name = "Acebot Odium", Path = "DATA", Directory = "acebot_settings.dat", Type = TYPE_FILE},
+
+    {Name = "Admin Client", Path = "DATA", Directory = "Elite", Type = TYPE_DIR},
+
+    {Name = "lesp", Path = "DATA", Directory = "lesp3_settings.txt", Type = TYPE_FILE},
+
+    {Name = "Safari", Path = "DATA", Directory = "crappens.txt", Type = TYPE_FILE},
+
+    {Name = "NeonHack", Path = "DATA", Directory = "neon_logged_ips.txt", Type = TYPE_FILE},
+
+    {Name = "AllahCheat", Path = "DATA", Directory = "AC", Type = TYPE_DIR},
+
+    {Name = "Ampbot", Path = "DATA", Directory = "Ampbot_convars.txt", Type = TYPE_FILE},
+    {Name = "Ampbot", Path = "DATA", Directory = "Ampbot_entities.txt", Type = TYPE_FILE},
+    {Name = "Ampbot", Path = "DATA", Directory = "Ampbot_friends.txt", Type = TYPE_FILE},
+
+    {Name = "AnXition", Path = "DATA", Directory = "AnXition", Type = TYPE_DIR},
+
+    {Name = "OubHack", Path = "DATA", Directory = "OubHack", Type = TYPE_DIR},
+
+    {Name = "Ares", Path = "DATA", Directory = "ares_ips.txt", Type = TYPE_FILE},
+
+    {Name = "Aah", Path = "DATA", Directory = "aah_keywords.txt", Type = TYPE_FILE},
+
+    {Name = "AutoAim (BenBoost)", Path = "DATA", Directory = "ac_ips.txt", Type = TYPE_FILE},
+
+    {Name = "b1g", Path = "DATA", Directory = "_b1g_settings.txt", Type = TYPE_FILE }
 }
